0d34a4612969aedfba448972d1886a4310ed99a1
jojoldu <jojoldu@gmail.com>
2017-12-25 18:09:22 +0900

    [2] 간단 레파지토리 테스트 코드 추가

--- src/main/java/com/jojoldu/webservice/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/webservice/domain/posts/Posts.java
@@ -1,17 +1,25 @@
 package com.jojoldu.webservice.domain.posts;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * Created by jojoldu@gmail.com on 2017. 12. 23.
  * Blog : http://jojoldu.tistory.com
  * Github : https://github.com/jojoldu
  */
-@NoArgsConstructor
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
 public class Posts {
 
@@ -19,5 +27,22 @@ public class Posts {
     @GeneratedValue
     private Long id;
 
+    @Column(length = 500, nullable = false)
+    private String title;
 
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+    private LocalDateTime createdTime;
+    private LocalDateTime updatedTime;
+
+    @Builder
+    public Posts(String title, String content, String author, LocalDateTime createdTime, LocalDateTime updatedTime) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.createdTime = createdTime;
+        this.updatedTime = updatedTime;
+    }
 }
--- src/test/java/com/jojoldu/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/webservice/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by jojoldu@gmail.com on 2017. 12. 23.
  * Blog : http://jojoldu.tistory.com
@@ -28,6 +34,23 @@ public class PostsRepositoryTest {
     @Test
     public void 게시글저장_불러오기() {
         //given
+        final LocalDateTime createdTime = LocalDateTime.of(2017, 12, 24, 14, 0);
+        final LocalDateTime updatedTime = LocalDateTime.of(2017, 12, 25, 14, 0);
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("jojoldu@gmail.com")
+                .createdTime(createdTime)
+                .updatedTime(updatedTime)
+                .build());
 
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+        assertThat(posts.getCreatedTime(), is(createdTime));
     }
 }
