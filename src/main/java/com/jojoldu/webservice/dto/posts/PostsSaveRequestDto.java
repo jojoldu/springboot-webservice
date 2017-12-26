365710c53ea93889c8ef25add18a3ddcbe1c03f9
jojoldu <jojoldu@gmail.com>
2017-12-27 08:45:47 +0900

    [2] Controller + DTO 추가

---
+++ src/main/java/com/jojoldu/webservice/dto/posts/PostsSaveRequestDto.java
@@ -0,0 +1,30 @@
+package com.jojoldu.webservice.dto.posts;
+
+import com.jojoldu.webservice.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 12. 27.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
--- src/main/java/com/jojoldu/webservice/web/WebRestController.java
+++ src/main/java/com/jojoldu/webservice/web/WebRestController.java
@@ -1,6 +1,11 @@
 package com.jojoldu.webservice.web;
 
+import com.jojoldu.webservice.domain.posts.PostsRepository;
+import com.jojoldu.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,10 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
