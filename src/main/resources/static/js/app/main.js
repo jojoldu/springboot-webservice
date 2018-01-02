72b15625c7ed61c719e5fa57010359f3ab5b86a7
jojoldu <jojoldu@gmail.com>
2018-01-03 08:44:54 +0900

    [3] 게시글 등록 js & hbs 완성

---
+++ src/main/resources/static/js/app/main.js
@@ -0,0 +1,38 @@
+/**
+ * Created by jojoldu@gmail.com on 2018. 1. 3.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+    },
+    save : function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            location.reload();
+        }).fail(function (error) {
+            alert(error);
+        });
+    }
+
+};
+
+main.init();
