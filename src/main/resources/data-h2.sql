79d26f208be39d46144cfd1a4ca444e168c13634
jojoldu <jojoldu@gmail.com>
2018-01-04 08:45:25 +0900

    [3] 입력 기능 완성 및 목록 출력 기능 환경 설정

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,2 @@
+insert into posts (title, author, content, created_date, modified_date) values ('테스트1', 'test1@gmail.com', '테스트1의 본문', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('테스트2', 'test2@gmail.com', '테스트2의 본문', now(), now());
